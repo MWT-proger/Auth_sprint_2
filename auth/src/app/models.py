9e555b3fbd3680ea95f367cf3bb4f758a852daa6
Андрей Белов <47903160+MWT-proger@users.noreply.github.com>
2022-04-22 22:26:01 +0300

    Update models.py

--- auth/src/app/models.py
+++ auth/src/app/models.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
-from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 
 from database import db
+from flask_security import RoleMixin, UserMixin
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class DateTimeMixin:
@@ -10,11 +12,30 @@ class DateTimeMixin:
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
 
-class User(db.Model, DateTimeMixin):
+class User(db.Model, DateTimeMixin, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     login = db.Column(db.String(128), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column("password", db.String(255), nullable=False)
-    active = db.Column(db.Boolean, default=True)
+    active = db.Column(db.Boolean(), default=True)
+    roles = db.relationship("Role", secondary="content.roles_users", backref=db.backref("users", lazy="dynamic"))
+
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = "roles"
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+
+class RolesUsers(db.Model):
+    __tablename__ = "roles_users"
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = db.Column("user_id", UUID(as_uuid=True), db.ForeignKey("users.id"))
+    role_id = db.Column("role_id", UUID(as_uuid=True), db.ForeignKey("roles.id"))
+
+    UniqueConstraint("user_id", "role_id", name="role_user_inx")
